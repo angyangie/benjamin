21b91dc31750ef460a5708186e87d33b1c33f06d
cjtafoya <cjtafoya@gmail.com>
2016-05-17 17:44:45 -0400

    testing .gitignore

---
+++ config/initializers/argyle.rb
@@ -0,0 +1,5 @@
+Argyle.configure do |config|
+  config.key = 'test_key'         # Public key from Plaid
+  config.secret = "test_secret"   # Secret from Plaid
+  config.customer_id = "test_id"  # Client ID from Plaid
+end
