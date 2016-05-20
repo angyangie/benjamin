cd7eba3f9e093c1eedc52b2a7df2266a5e442a15
angyangie <angie.ramirez12@gmail.com>
2016-05-20 18:17:25 -0400

    Error messages on login/signup done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-     @user = User.find_by(email: params[:user][:email])
+   @user = User.find_by(email: params[:user][:email])
      if @user && @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id
        redirect_to @user
      else
-       flash[:error] = "Invalid Username or Password. Please try again."
-       redirect_to login_path
-     end
+      flash[:message] = "You have entered the wrong email or password. Please try again."
+      redirect_to login_path
+    end
   end
 
   def destroy
     session.delete :user_id
+    flash[:notice] = "You have successfully logged out."
+    redirect_to login_path
   end
 
-private
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
+
+
+
