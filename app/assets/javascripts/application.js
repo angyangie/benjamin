a07611ea71e2aade0f9f9efc40f4d6592f7bb818
Michael Handwerker <michael.handwerker@gmail.com>
2016-05-24 17:46:22 -0400

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require bootstrap-sprockets
-//= require d3
+
 
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require d3
 //= require turbolinks
 
 //= require application.js
