8c1920ed0fdbcda3b2a36a95412336d984aa291a
cjtafoya <cjtafoya@gmail.com>
2016-05-24 13:46:41 -0400

    changed table to divs

--- app/assets/javascripts/income_statement.js
+++ app/assets/javascripts/income_statement.js
@@ -4,58 +4,58 @@ $(function(){
 })
 
 function hideYoKids(){
-  $('tr.subcategory').hide()
-  $('tr.child').hide()
-  $('tr.leaves').hide()
+  $('div.subcategory').hide()
+  $('div.child').hide()
+  $('div.leaves').hide()
 }
 
 function showSubcategories(){
-  $('tr.root.income').on('click', function(){
+  $('div.root.income').on('click', function(){
     $(this).addClass('success')
-    $('tr.root.expenses').addClass('danger')
-    $('tr.subcategory.income').slideToggle()
+    $('div.root.expenses').addClass('danger')
+    $('div.subcategory.income').slideToggle()
   })
-  $('tr.root.expenses').on('click', function(){
+  $('div.root.expenses').on('click', function(){
     $(this).addClass('danger')
-    $('tr.root.income').addClass('success')
-    $('tr.subcategory.expenses').slideToggle()
+    $('div.root.income').addClass('success')
+    $('div.subcategory.expenses').slideToggle()
   })
-  $('tr#fixed').on('click', function(){
-    $('tr.child.fixed').slideToggle()
+  $('div#fixed').on('click', function(){
+    $('div.child.fixed').slideToggle()
     addActive()
   })
-  $('tr#variable').on('click', function(){
-    $('tr.child.variable').slideToggle()
+  $('div#variable').on('click', function(){
+    $('div.child.variable').slideToggle()
     addActive()
   })
-  $('tr#discretionary').on('click', function(){
-    $('tr.child.discretionary').slideToggle()
+  $('div#discretionary').on('click', function(){
+    $('div.child.discretionary').slideToggle()
     addActive()
   })
-  $('tr#life').on('click', function(){
-    $('tr.child.life').slideToggle()
+  $('div#life').on('click', function(){
+    $('div.child.life').slideToggle()
     addActive()
   })
-  $('tr#other-exp').on('click', function(){
-    $('tr.child.other-exp').slideToggle()
+  $('div#other-exp').on('click', function(){
+    $('div.child.other-exp').slideToggle()
     addActive()
   })
-  $('tr#total-food').on('click', function(){
+  $('div#total-food').on('click', function(){
     $(this).toggleClass('active')
-    $('tr.leaves.food').slideToggle()
+    $('div.leaves.food').slideToggle()
   })
 }
 
 function addActive(){
-  $('tr#fixed').addClass('active')
-  $('tr#variable').addClass('active')
-  $('tr#discretionary').addClass('active')
-  $('tr#life').addClass('active')
-  $('tr#other-exp').addClass('active')
+  $('div#fixed').addClass('active')
+  $('div#variable').addClass('active')
+  $('div#discretionary').addClass('active')
+  $('div#life').addClass('active')
+  $('div#other-exp').addClass('active')
 }
 
 function childCheck(){
-  if($('tr.child').attr('style') === "display: none;") {
+  if($('div.child').attr('style') === "display: none;") {
     addActive()
   } 
 }
