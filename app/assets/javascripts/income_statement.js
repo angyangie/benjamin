990ef31de1dd201661fc1f3de09848379c87a170
cjtafoya <cjtafoya@gmail.com>
2016-05-23 23:24:03 -0400

    fixed table

--- app/assets/javascripts/income_statement.js
+++ app/assets/javascripts/income_statement.js
@@ -1,31 +1,61 @@
 $(function(){
   hideYoKids()
   showSubcategories()
-  // hoverRoot()
 })
 
 function hideYoKids(){
   $('tr.subcategory').hide()
+  $('tr.child').hide()
+  $('tr.leaves').hide()
 }
 
 function showSubcategories(){
   $('tr.root.income').on('click', function(){
-    $(this).toggleClass('success')
+    $(this).addClass('success')
+    $('tr.root.expenses').addClass('danger')
     $('tr.subcategory.income').slideToggle()
   })
   $('tr.root.expenses').on('click', function(){
-    $(this).toggleClass('danger')
+    $(this).addClass('danger')
+    $('tr.root.income').addClass('success')
     $('tr.subcategory.expenses').slideToggle()
+  })
+  $('tr#fixed').on('click', function(){
+    $('tr.child.fixed').slideToggle()
+    addActive()
+  })
+  $('tr#variable').on('click', function(){
+    $('tr.child.variable').slideToggle()
+    addActive()
+  })
+  $('tr#discretionary').on('click', function(){
+    $('tr.child.discretionary').slideToggle()
+    addActive()
+  })
+  $('tr#life').on('click', function(){
+    $('tr.child.life').slideToggle()
+    addActive()
+  })
+  $('tr#other-exp').on('click', function(){
+    $('tr.child.other-exp').slideToggle()
+    addActive()
+  })
+  $('tr#total-food').on('click', function(){
+    $(this).toggleClass('active')
+    $('tr.leaves.food').slideToggle()
   })
 }
 
-function hoverRoot(){
-  $('tr.root.income').hover(
-    function(){$(this).addClass('success')},
-    function(){$(this).removeClass('success')}
-  )
-  $('tr.root.expenses').hover(
-    function(){$(this).addClass('danger')},
-    function(){$(this).removeClass('danger')}
-  )  
+function addActive(){
+  $('tr#fixed').addClass('active')
+  $('tr#variable').addClass('active')
+  $('tr#discretionary').addClass('active')
+  $('tr#life').addClass('active')
+  $('tr#other-exp').addClass('active')
+}
+
+function childCheck(){
+  if($('tr.child').attr('style') === "display: none;") {
+    addActive()
+  } 
 }
