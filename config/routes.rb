52b2c4432219143a7ce93ae93177e34c1ccdace9
cjtafoya <cjtafoya@gmail.com>
2016-05-26 00:09:38 -0400

    still here at 1209am

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,28 @@ Rails.application.routes.draw do
 
   root 'home#index'
   post '/users/piechart', to: 'users#piechart'
+
+
+
   resources :users, except: [:new, :create] do
     resources :transactions, only: [:index, :show, :edit, :update]
     resources :accounts, only: [:index]
+    resources :budgets, except: [:index]
   end
 
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create', as: 'create'
+
+  # This seems redundant
   get '/update/:id', to: 'users#edit', as: 'edit'
   patch '/update/:id', to: 'users#update', as: 'user_update'
-
-  resources :budgets, except: [:index]
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  post '/users/accounts', to: 'plaidapi#add_account'
-  patch '/user/:id/update_accounts', to: 'plaidapi#update_accounts'
+  post '/users/:id/add_account', to: 'plaidapi#add_account'
+  patch '/user/:id/update_accounts', to: 'plaidapi#update_accounts'  
 
   get 'graph/index'
   get 'graph/data', :defaults => { :format => 'json' }
