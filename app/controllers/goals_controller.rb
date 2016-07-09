779f641d8ff13df2637ad827e36f647008110735
angyangie <angie.ramirez12@gmail.com>
2016-07-09 16:24:16 -0400

    route to user path in goals view working

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,8 +8,10 @@ class GoalsController < ApplicationController
 
   def create
     @user = current_user
-    @goal = Goal.find_or_create_by(product_url: params[:goal][:product_url], user_id: user.id)
-    redirect_to goal_path(@user)
+    @goal = Goal.find_or_create_by(product_url: params[:goal][:product_url], user_id: current_user.id)
+    redirect_to goals_path(@user)
+
+
   end 
 
   def show 
